85846d2abfe1d149eebe7dc372d35efad505c511
krutoo <dima.krutoo@yandex.ru>
2020-12-21 11:05:11 +0500

    demo updated

--- demo/index.jsx
+++ demo/index.jsx
@@ -7,15 +7,33 @@ window.addEventListener('DOMContentLoaded', () => {
   render(<App />, document.querySelector('#root'));
 });
 
-const App = () => {
+const App = () => (
+  <>
+    <h2>Input mask tests</h2>
+    <DemoInput
+      mask='+7 (___) ___-__-__'
+      label='Номер телефона'
+    />
+    <DemoInput
+      mask='____ ______'
+      label='Серия и номер паспорта'
+    />
+    <DemoInput
+      mask='____ ____ ____ ____'
+      label='Номер карты'
+    />
+  </>
+);
+
+const DemoInput = ({ mask, label }) => {
   const ref = useRef();
 
-  useInputMask(ref, { mask: '+7 (___) ___-__-__' });
+  useInputMask(ref, { mask });
 
   return (
-    <>
-      <h2>Input mask test</h2>
+    <div className='demo-input'>
+      {label && (<label>{label}</label>)}
       <input ref={ref} />
-    </>
+    </div>
   );
 };
