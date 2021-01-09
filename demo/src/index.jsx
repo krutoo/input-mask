8916c95140d7020c3556e7cfc9bff021a98987ed
krutoo <dima.krutoo@yandex.ru>
2021-01-09 19:54:44 +0500

    demo changed

--- demo/src/index.jsx
+++ demo/src/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { render } from 'react-dom';
 import { MaskMixin } from '../../src/react';
+import { InputMask } from '../../src/dom';
 import './index.css';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -11,6 +12,8 @@ const variants = [
   {
     mask: '+7 (___) ___-__-__',
     label: 'Номер телефона',
+    type: 'tel',
+    id: 'phone',
   },
   {
     mask: '__ __ ______',
@@ -19,6 +22,7 @@ const variants = [
   {
     mask: '____ ____ ____ ____',
     label: 'Номер карты',
+    autoComplete: 'cc-number',
   },
   {
     mask: '__ / __ / ____',
@@ -28,18 +32,54 @@ const variants = [
 
 const App = () => (
   <>
-    <h2>Тесты масок (React)</h2>
+    <h3 data-primary>Vanilla JS solution</h3>
+    {variants.map((variant, i) => (
+      <DemoVanilla key={i} {...variant} />
+    ))}
+
+    <h3>React solution (component mixin)</h3>
     {variants.map((variant, i) => (
       <Demo key={i} {...variant} />
     ))}
 
-    <h3>Stateful components (value change)</h3>
+    <h3>React solution with value prop change</h3>
     <DemoStateful
       mask='+7 (___) ___-__-__'
       label='Номер телефона'
     />
   </>
 );
+
+const DemoVanilla = ({ mask, label, ...inputProps }) => {
+  const [inputMask, setInputMask] = useState();
+  const ref = useRef();
+
+  useEffect(() => {
+    const m = InputMask(ref.current, {
+      mask,
+      onChange: data => console.log(data),
+    });
+
+    setInputMask(m);
+
+    return () => m.disable();
+  }, []);
+
+  return (
+    <div className='demo-block'>
+      {label && (<label>{label}</label>)}
+      <input
+        ref={ref}
+        placeholder={mask}
+        {...inputProps}
+      />
+      <div className="controls">
+        <button onClick={() => inputMask?.setValue('')}>Clean</button>
+        <button onClick={() => inputMask?.setValue('0'.repeat(99))}>Fill by zero</button>
+      </div>
+    </div>
+  );
+};
 
 const Demo = ({ mask, label }) => (
   <div className='demo-block'>
