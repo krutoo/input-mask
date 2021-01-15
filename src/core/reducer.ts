5193518ab353a37b16fc0c08221ee54ba58d5f8f
krutoo <dima.krutoo@yandex.ru>
2021-01-15 22:50:42 +0500

    experimental: remove cutting value

--- src/core/reducer.ts
+++ src/core/reducer.ts
@@ -147,12 +147,8 @@ export const createReducer = ({ mask, pattern, placeholder }: ReducerOptions): R
       }
     }
 
-    // удаляем остаточную часть маски после введенного значения
-    while (result.length > 0 && !Index.isPlace(result.length - 1)) {
-      result = result.slice(0, -1);
-    }
-
     // предотвращаем возможность удалить часть маски до вводимого места
+    // @todo удалить это после появления возможности передать функцию предобработки значения
     if (result.length < placeIndices[0]) {
       result = mask.slice(0, placeIndices[0]);
     }
